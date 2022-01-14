c058ae0961f21bc4bb64ef146d8156c65b7f38b2
nirob897 <97749887+nirob897@users.noreply.github.com>
2022-01-15 01:10:45 +0600

    Create nilbanr.py
    apt update
    apt upgrade
    apt install git
    git clone https://github.com/nirob897/nilbanr.git
    cd nilbanr.git
    bash requirement.sh
    bash t-ban.sh

---
+++ nilbanr.py
@@ -0,0 +1,39 @@
+# Color --------
+B="\033[0;30m" # Black
+R="\033[1;31m" # Red
+G="\033[1;32m" # Green
+Y="\033[1;33m" # Yellow
+Bl="\033[1;34m" # Blue
+P="\033[1;35m" # Purple
+C="\033[1;36m" # Cyan
+W="\033[1;37m" # Whit
+# End Color -------
+ 
+# Banner ---------
+def banner():
+ 
+    print(G+'  ____      _                 ____             _ ')
+    print(G+' / ___|   _| |__   ___ _ __  |  _ \  __ _ _ __| | __')
+    print(G+'''| |  | | | | '_ \ / _ \ '__| | | | |/ _` | '__| |/ / ''')
+    print(G+'| |__| |_| | |_) |  __/ |    | |_| | (_| | |  |   < ')
+    print(G+' \____\__, |_.__/ \___|_|    |____/ \__,_|_|  |_|\_\ ', R+'EnCode & DeCode Hash')
+    print(G+'      |___/                                         ',R+'~ Code By Cyber Dark ~')
+    print('')
+    print(f"{Y}~ Code By Cyber Dark")
+    print(Y+'~ Instagram: @CyberDarkCrew')
+    print(Y+'~ Facebook : @CyberDarkCrew')
+    print(Y+'~ Twitter  : @CyberDarkCrew')
+    print(Y+'~ TikTok   : @CyberDarkCrew')
+    print(C+'''
+    ~ Cyber Dark Team ~
+        Telegram : 
+            Mr.Dark : @L_N_X_0
+            Mixail  : @M3x4il
+            Hosin   : @Hosinballo
+            Rasas   : @rasas772
+            Omer    : @nice_nice
+    ''')
+    
+banner()
+ 
+# EndBanner -----------
